9beb82c841595b54b95da7922e9ce35f34221707
= <=>
2020-12-11 10:43:11 +0100

    estructura

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -7,6 +8,7 @@ import { RegisterComponent } from './register/register.component';
 const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: "login", component: LoginComponent},
   { path: "task", component: TaskDetailsComponent},
   { path: "register", component: RegisterComponent},
   { path: "**", component: HomeComponent},
