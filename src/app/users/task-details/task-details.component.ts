e1e15c6f3bc9495a31ab467576e5bc94fd5beb6b
= <=>
2020-12-11 13:24:00 +0100

    canvis

--- src/app/users/task-details/task-details.component.ts
+++ src/app/users/task-details/task-details.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-details.component.scss']
 })
 export class TaskDetailsComponent implements OnInit {
+  public hora: number = 0;
+  public minuto: number = 0;
+  public segundos: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  start(){
+    setInterval(()=>{
+      this.segundos+=1;
+      if(this.segundos == 60){
+        this.segundos = 0;
+        this.minuto +=1;
+        if(this.minuto == 60){
+          this.minuto=0;
+          this.hora+=1;
+          if(this.hora = 24){
+            this.hora =0;
+          }
+        }
+      }
+    }, 1000);
+  }
+  pausar(){
+
+  }
+  parar(){
   }
 
 }
