cfc20f07cab0f851526881430ea0b10bca364d7f
= <=>
2020-12-11 10:38:41 +0100

    estructura

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TaskDetailsComponent } from './users/task-details/task-details.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: "task", component: TaskDetailsComponent},
+  { path: "register", component: RegisterComponent},
+  { path: "**", component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
